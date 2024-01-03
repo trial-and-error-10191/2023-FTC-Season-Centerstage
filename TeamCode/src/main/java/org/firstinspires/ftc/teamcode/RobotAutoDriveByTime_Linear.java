19f5f62384e350eef0eb126df96bdfc9e04b98c1
Cait <kahvidsachi@gmail.com>
2024-01-02 19:54:51 -0600

    On this file, i commented out the disable thing and attempted to create an autonomous.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByTime_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByTime_Linear.java
@@ -59,14 +59,16 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 //@Disabled
 public class RobotAutoDriveByTime_Linear extends LinearOpMode {
 
-    /* Declare OpMode members. */
-    private DcMotor         leftDrive   = null;
-    private DcMotor         rightDrive  = null;
 
     private ElapsedTime     runtime = new ElapsedTime();
 
     static final double MAX_POS     =  1.0;     // Maximum rotational position
     static final double MIN_POS     =  0.0;     // Minimum rotational position
+
+    private DcMotor         leftFrontDrive   = null;
+    private DcMotor         rightFrontDrive  = null;
+    private DcMotor         leftBackDrive = null;
+    private DcMotor         rightBackDrive = null;
 
     // Define class members
     Servo servo;
@@ -82,14 +84,18 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
     public void runOpMode() {
 
         // Initialize the drive system variables.
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Status", "Ready to run");    //
@@ -101,7 +107,8 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         // Step through each leg of the path, ensuring that the Auto mode has not been stopped along the way
 
         // Step 1:  2 front motors spin for 3 seconds
-        leftDrive.setPower(FORWARD_SPEED);
+        leftFrontDrive.setPower(FORWARD_SPEED);
+        rightFrontDrive.setPower(FORWARD_SPEED);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 3.0)) {
             telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
@@ -109,7 +116,8 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         }
 
         // Step 2:  first servo spin from min to max for 3.0 seconds
-        leftDrive.setPower(0);
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
         servo.setPosition(MAX_POS);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 3.0)) {
@@ -119,7 +127,8 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
 
         // Step 3:  2 back motors spin for 3 seconds
         servo.setPosition(MIN_POS);
-        rightDrive.setPower(FORWARD_SPEED);
+        rightBackDrive.setPower(FORWARD_SPEED);
+        leftBackDrive.setPower(FORWARD_SPEED);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 3.0)) {
             telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
@@ -127,7 +136,8 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         }
 
         // Step 4:  second servo spin from min to max for 3.0 seconds
-        rightDrive.setPower(0);
+        rightBackDrive.setPower(0);
+        leftBackDrive.setPower(0);
         servo_2.setPosition(MAX_POS);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 3.0)) {
@@ -136,7 +146,7 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         }
 
         /*
-        // Step 3:  Drive Backward for 1 Second
+        // Step 3:  Drive Backward for 3 Seconds
         leftDrive.setPower(-FORWARD_SPEED);
         rightDrive.setPower(-FORWARD_SPEED);
         leftDrive.setPower(TURN_SPEED);
